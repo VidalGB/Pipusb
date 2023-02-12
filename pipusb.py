a5b3e3d0fcfa807c5f76695b823f9149703b3f86
VidalGB <manuelgarcia0660@gmail.com>
2023-02-12 10:39:38 -0300

    Added notification system

--- pipusb.py
+++ pipusb.py
@@ -3,41 +3,29 @@
 
 # Imports
 import argparse
-import sys
 import usb
 from playsound import playsound
 import psutil
 import time
-
-def ensambler_id(*args):
-  parts = list()
-  for part in args:
-    part = part.split('x')
-    part = part[1]
-    len_part = len(part)
-    if len_part < 4:
-      more = 4 - len_part
-      part = '0'*more + part
-    parts.append(part)
-
-  return f"{parts[0]}:{parts[1]}"
+from notifypy import Notify
 
 def list_devices():
   busses = usb.busses()
   num_dev = int()
-  id_dev = dict()
   for bus in busses:
     devices = bus.devices
-    for num, dev in enumerate(devices):
-      id_vendor = "%s"%(hex(dev.idVendor))
-      id_product = "%s"%(hex(dev.idProduct))
-      id_dev[num + num_dev + 1] = ensambler_id(id_vendor, id_product)
     num_dev = len(devices) + num_dev
 
-  return num_dev, id_dev
+  return num_dev
 
 def play_sound(sound):
   playsound(sound)
+
+def notification(text, sound):
+  notification = Notify(default_application_name="pipusb")
+  notification.title = text
+  notification.audio = sound
+  notification.send()
 
 # Main function
 def main():
@@ -49,24 +37,35 @@ def main():
   parser.add_argument('-v', '--version', action = 'version', version = '%(prog)s 1.0', help = "show program's version number and exit.")
 
 # Input sound
-  parser.add_argument("-i","--input", type = str, default = './default/input_sound.mp3', help = "file to play of the input USB.")
+  parser.add_argument("-i","--input", type = str, default = './default/input_sound.mp3', help = "file to play when inserting USB. (only .wav files are supported)")
 
 # Output sound
-  parser.add_argument("-o", "--output", type = str, default = './default/input_sound.mp3', help = "file to play of the output USB.")
+  parser.add_argument("-o", "--output", type = str, default = './default/input_sound.mp3', help = "file to play when removing USB. (only .wav files are supported)")
 
-# DeepL translator argument (free)
-  parser.add_argument("-e", "--energize", action = 'store_true', help = 'energize.')
+# dee
+  parser.add_argument("-c", "--charger", action = 'store_true', help = 'detect charger.')
+
+# ede
+  parser.add_argument("-n", "--notification", action = 'store_true', help = 'show notification on USB detection.')
   args = parser.parse_args()
   
   old_devices = list_devices()
   while True:
     time.sleep(0.01)
     new_devices = list_devices()
-    if new_devices[0] > old_devices[0]:
-      play_sound(args.input)
-    if new_devices[0] < old_devices[0]:
-      play_sound(args.output)
-    if args.energize:
+    if new_devices > old_devices:
+      if args.notification:
+        notification("USB Connected", args.input)
+      else:
+        play_sound(args.input)
+
+    if new_devices < old_devices:
+      if args.notification:
+        notification("USB Disconnected", args.output)
+      else:
+        play_sound(args.output)
+
+    if args.charger:
       battery = psutil.sensors_battery()
       power = battery.power_plugged
       if power:
