1f0b15a068860e792d996c266fb2e84726077f48
VidalGB <manuelgarcia0660@gmail.com>
2023-02-11 22:07:09 -0300

    Add functions to filter USBs and play sound

--- pipusb.py
+++ pipusb.py
@@ -4,6 +4,38 @@
 # Imports
 import argparse
 import sys
+import usb
+from playsound import playsound
+
+def ensambler_id(*args):
+  parts = list()
+  for part in args:
+    part = part.split('x')
+    part = part[1]
+    len_part = len(part)
+    if len_part < 4:
+      more = 4 - len_part
+      part = '0'*more + part
+    parts.append(part)
+
+  return f"{parts[0]}:{parts[1]}"
+
+def list_devices():
+  busses = usb.busses()
+  num_dev = int()
+  id_dev = dict()
+  for bus in busses:
+    devices = bus.devices
+    for num, dev in enumerate(devices):
+      id_vendor = "%s"%(hex(dev.idVendor))
+      id_product = "%s"%(hex(dev.idProduct))
+      id_dev[num + num_dev + 1] = ensambler_id(id_vendor, id_product)
+    num_dev = len(devices) + num_dev
+
+  return num_dev, id_dev
+
+def play_sound(sound):
+  playsound(sound)
 
 # Main function
 def main():
@@ -22,8 +54,9 @@ def main():
 
 # DeepL translator argument (free)
   parser.add_argument("-e", "--energize", action = 'store_true', help = 'energize.')
-
   args = parser.parse_args()
+  
+  old_devices = list_devices()
 
 # Check script main
 if __name__ == '__main__':
